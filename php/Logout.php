f7fa13f456bab78244b6806c5df0adfaba58d066
Ly Nguyen Quoc Hung <126126243+huntly22122003@users.noreply.github.com>
2025-07-25 01:42:38 +0700

    Delete php file

--- php/Logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Khởi động phiên
-session_destroy(); // Xóa tất cả các biến phiên và kết thúc phiên đăng nhập
-header("Location:../html/PJ.php"); // Chuyển hướng người dùng đến trang đăng nhập
-exit();
-?>
